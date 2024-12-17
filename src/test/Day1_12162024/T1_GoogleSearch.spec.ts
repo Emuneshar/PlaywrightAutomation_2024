518abb0de997431b98a859cee0b617329f4abf3b
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-16 22:20:16 -0500

    Added comments

--- src/test/Day1_12162024/T1_GoogleSearch.spec.ts
+++ src/test/Day1_12162024/T1_GoogleSearch.spec.ts
@@ -11,12 +11,13 @@ test('Search for bmw on google', async () => {
     await page.locator("xpath=//*[@name='q']").fill("bmw")
     await page.locator("xpath=//*[@name='btnK']").nth(0).click()
     await page.waitForTimeout(5000)
-})
+}) // end of test 1 
 
 
 test('Capture search number for BMW', async () => {
     let result = page.locator("xpath=//*[@id='result-stats']")
     let arrayResult = result.toString().split(" ")
     console.log("Search number for BMW: " + arrayResult[1])
-})
+}) // end of test 2
+
 
