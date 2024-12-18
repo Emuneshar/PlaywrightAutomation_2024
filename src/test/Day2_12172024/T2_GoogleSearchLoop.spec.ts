781977f61d26decae0b6533258484e34dbc2d797
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-17 21:50:40 -0500

    added current item to console log

---
+++ src/test/Day2_12172024/T2_GoogleSearchLoop.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, Page } from '@playwright/test';
+
+//declare global page variable
+let page: Page
+test.beforeAll(async ({ browser }) => {
+   page = await browser.newPage()
+})//end of beforeAll
+
+let cars = Array<string>()
+cars.push("BMW")
+cars.push("Audi")
+cars.push("Chevy")
+
+
+
+
+test('Search for cars on google', async () => {
+    for(let i = 0; i < cars.length; i++){
+        await page.goto("https://www.google.com")
+        await page.locator("xpath=//*[@name='q']").fill(cars[i])
+        await page.locator("xpath=//*[@name='btnK']").nth(0).click()   
+        let result = await page.locator("xpath=//*[@id='result-stats']").textContent()
+        let arrayResult = result.toString().split(" ")
+        console.log("Search number for " + cars[i] + ": " + arrayResult[1])
+    }
+}) 
+
