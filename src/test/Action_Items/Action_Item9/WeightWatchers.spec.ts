f51fd71423bcb289cf6c2c3a5e7ecd976b342b0a
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-19 16:08:49 -0500

    added code to maximize the screen with comments

--- src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
+++ src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
@@ -8,9 +8,18 @@ test.beforeAll(async ({browser}) => {
     page = await browser.newPage()
 }) // end of before all
 
+let zipCodes = Array<string>() // Create an array list of mock data
+
+// Added zipcodes to the list 
+zipCodes.push("90001")
+zipCodes.push("11001")
+zipCodes.push("77010")
+
+
 test('Go to weight watchers site', async() => {
+    await page.setViewportSize({width:1920, height:1032}) // attempted to mximize the screen
     await page.waitForTimeout(6000) // Added time to allow for the screen to maximize window
-    await page.goto("https://www.weightwatchers.com")
+    await page.goto("https://www.weightwatchers.com") // Navigate to the weight watchers site
     
     
     await page.waitForTimeout(6000) // slowed the script to see the results
