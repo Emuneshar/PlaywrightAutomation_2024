3ea781f7c861d8687935c33517d72708dbce1498
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-19 16:41:16 -0500

    added comments to the if statements

--- src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
+++ src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
@@ -12,6 +12,7 @@ let url = "https://www.weightwatchers.com/us/find-a-workshop"
 let xpathInPerson = "xpath=//*[text() = 'In-Person']"
 let xpathLocationSearch = "xpath=//*[@id = 'location-search']"
 let xpathRightArrow = "xpath=//*[@class = 'rightArrow-C_sUu']"
+let xpathStudioLink = "xpath=//*[@class = 'wrapperLink-rmsRn']"
 let zipCodes = Array<string>() // Create an array list of mock data
 
 // Added zipcodes to the list 
@@ -24,15 +25,29 @@ test('Go to weight watchers site', async() => {
     await page.setViewportSize({width:1920, height:1032}) // attempted to mximize the screen
     await page.waitForTimeout(6000) // Added time to allow for the screen to maximize window
     
+    for(let i = 0; i < zipCodes.length; i++){
+        await page.goto(url) // Navigate to the weight watchers site
+        await page.waitForTimeout(2000) // slowed the automation to see the script run
+        await page.locator(xpathInPerson).nth(0).click() // click on in person
+        await page.locator(xpathLocationSearch).click() // click on location search
+        await page.locator(xpathLocationSearch).clear() // clear zip code field
+        await page.locator(xpathLocationSearch).fill("11001") // sent in zipcode mock data
+        await page.locator(xpathRightArrow).click() // click on continue button
+
+        if(i === 0)( // if this is the first iteration, click on the second link
+            await page.locator(xpathStudioLink).nth(1).click()
+        )
+        else if(i ===1){ // if this is the second iteration, click on the third link
+            await page.locator(xpathStudioLink).nth(2).click()
+        }
+        else{ // if this is the third iteration, click on the first link
+            await page.locator(xpathStudioLink).nth(0).click()
+        }
+
+        
+        await page.waitForTimeout(2000)
+    }
     
-    await page.goto(url) // Navigate to the weight watchers site
-    await page.waitForTimeout(2000) // slowed the automation to see the script run
-    await page.locator(xpathInPerson).nth(0).click() // click on in person
-    await page.locator(xpathLocationSearch).click() // click on location search
-    await page.locator(xpathLocationSearch).clear() // clear zip code field
-    await page.locator(xpathLocationSearch).fill("11001") // sent in zipcode mock data
-    await page.locator(xpathRightArrow).click() // click on continue button
-    await page.waitForTimeout(2000)
 
     
     
