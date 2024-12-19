3757ec09c166467ecf11c04097acac3a69e4e35a
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-19 16:10:52 -0500

    changed url to navigate to

--- src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
+++ src/test/Action_Items/Action_Item9/WeightWatchers.spec.ts
@@ -19,7 +19,7 @@ zipCodes.push("77010")
 test('Go to weight watchers site', async() => {
     await page.setViewportSize({width:1920, height:1032}) // attempted to mximize the screen
     await page.waitForTimeout(6000) // Added time to allow for the screen to maximize window
-    await page.goto("https://www.weightwatchers.com") // Navigate to the weight watchers site
+    await page.goto("https://www.weightwatchers.com/us/find-a-workshop") // Navigate to the weight watchers site
     
     
     await page.waitForTimeout(6000) // slowed the script to see the results
