0a9b61f51157a266153c22ebb6d546566821d1e7
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-23 22:08:00 -0500

    added reusable methods

---
+++ src/main/Reusable_Methods.ts
@@ -0,0 +1,24 @@
+import {test, expect, Page} from "@playwright/test";
+
+
+// create a function 
+export async function click(page: Page, xpath: string, elementName: string){
+    console.log("Click on " + elementName)
+    await page.locator("xpath="+ xpath).click()
+}
+
+export async function clickByIndex(page: Page, xpath: string, index: number, elementName: string){
+    console.log("CLick on "+ elementName)
+    await page.locator("xpath=" + xpath).nth(index).click()
+}
+
+export async function captureText(page: Page, xpath: string, elementName: string){
+    console.log("Capture text on "+elementName)
+    let result = await page.locator("xpath=" + xpath).textContent()
+    return result
+}
+
+export async function sendKeys(page : Page, xpath: string, text: string, elementName: string){
+    console.log("Sneding keys to " + elementName)
+    await page.locator("xpath=" + xpath).fill(text)
+}
