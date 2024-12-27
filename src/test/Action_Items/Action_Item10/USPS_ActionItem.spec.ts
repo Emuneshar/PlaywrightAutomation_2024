530304de284be4ef588fd1c34b06ad824a116a9a
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-26 23:17:10 -0500

    finished assignment 1

--- src/test/Action_Items/Action_Item10/USPS_ActionItem.spec.ts
+++ src/test/Action_Items/Action_Item10/USPS_ActionItem.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect, Page} from "@playwright/test"
-import { click, clickByIndex, mouseHover, mouseHoverNth } from "../../../main/Reusable_Methods"
+import { captureText, click, clickByIndex, mouseHover, mouseHoverNth, scrollByPixel, sendKeys } from "../../../main/Reusable_Methods"
 
 let page: Page
 test.beforeAll(async ({browser}) => {
@@ -10,6 +10,13 @@ const url = "https://www.usps.com/"
 const xpathShop = "//*[@class = 'menuitem']"
 const xpathStamps = "//*[@class = 'tool-stamps']"
 const xpathStampButton = "//*[@class = 'checkbox-label']"
+const xpathFirstStamp = "//*[@class = 'col-6 col-md-4 results-per-page ']"
+const xpathAddToCart = "//*[@id = 'addToCartVisBtn122104']"
+const xpathViewCart = "//*[@class= 'button--primary button--white']"
+const xpathQuantity = "//*[@class = 'col-8 form-control']"
+const xpathUpdate = "//*[@id = 'atg_store_update']"
+const xpathInfo = "//*[@class = 'prod-info-detail']"
+
 
 test("Capture Stamp information under the item section", async () =>{
     await page.goto(url)
@@ -19,5 +26,14 @@ test("Capture Stamp information under the item section", async () =>{
     await clickByIndex(page, xpathStamps, 1,  "Stamps")
     await clickByIndex(page, xpathStampButton, 0,"Stamp Button")
     await clickByIndex(page, xpathStampButton, 4, "Additional Postage")
+    await scrollByPixel(page, 0, 400, "Scrolling")
+    await clickByIndex(page, xpathFirstStamp, 0, "First Stamp")
+    await click(page, xpathAddToCart, "Add to Cart")
+    await click(page, xpathViewCart, "View Cart")
+    await sendKeys(page, xpathQuantity, "2", "Quantity")
+    await click(page, xpathUpdate, "Update")
     await page.waitForTimeout(2000)
+    let text = await captureText(page,xpathInfo, "Info")
+    let trimmedText = text.trim()
+    console.log(trimmedText)
 })
