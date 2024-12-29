a98f56842cfd41e53b742676af3b9056d4bc3105
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2024-12-28 20:32:08 -0500

    Added more comments

--- src/test/Action_Items/Action_Item10/UseCase_NorthWesternMutual.spec.ts
+++ src/test/Action_Items/Action_Item10/UseCase_NorthWesternMutual.spec.ts
@@ -20,12 +20,14 @@ test("verify text on site @ai", async() => {
     await page.goto(url) // Goes to the url
     await mouseHover(page, xpathFinancialPlanning, "Financial Planning") // Hovers over the financial planning menu
     await click(page, xpathEstatePlanning, "Estate Planning") // Clicks on the estate planning option from the previous menu
-    let text = await captureText(page, xpathAboutPlanning, "About planning")
-    console.log(text)
+    let text = await captureText(page, xpathAboutPlanning, "About planning") // gets the text from the northwestern mutual site
+    console.log(text) // prints out the text for user to see
+    
+    // This if statement will compare the text on the site to what we expect it to be. We use 3 equal signs for strict comparison. I am using this in place of Assert Equal in Selenium
     if(expectedText === text){
-        console.log("Text was a match")
+        console.log("Text was a match") // prints out that it was a match if the text matches
     }
     else{
-        console.log("Text was not a match")
+        console.log("Text was not a match") // prints out that the text wasn't a match if it doesn't match
     }
-})
+}) // End of test
